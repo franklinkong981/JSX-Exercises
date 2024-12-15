bd1435eb0556152e0f7d4cdcea2e1cbd7a6a61ba
Franklin Kong <franklinkong981@gmail.com>
2024-12-15 13:03:47 -0800

    Styled part 2

--- part-2/App.js
+++ part-2/App.js
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div>
+    <div className="px-2 justify-content-center">
       <Tweet username="franklinkong981" name="Franklin Kong" message="Hello React!" date="12/15/2024"/>
       <Tweet username="jeffreykong981" name="Jeffrey Kong" message="I really like McDonald's especially the Big Mac" date="7/24/2023"/>
       <Tweet username="christhegreat" name="Chris Chen" message="I am so good at Minecraft!" date="5/30/2024"/>
--- part-2/Tweet.js
+++ part-2/Tweet.js
@@ -1,9 +1,9 @@
 const Tweet = ({username, name, date, message}) => {
   return (
-    <div>
-      <h2>Tweet by {name} <em>@{username}</em></h2>
-      <p>{message}</p>
-      <small>Tweeted on{date}</small>
+    <div className="px-2 border border-primary rounded">
+      <h2 className="display-4 text-center">Tweet by {name} <span className="badge badge-secondary">@{username}</span></h2>
+      <h4 className="text-success text-center font-weight-bold">{message}</h4>
+      <p className="fs-6 text-center">Tweeted on {date}</p>
     </div>
   );
 };
